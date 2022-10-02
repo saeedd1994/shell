9d0770d3d351db41fffa2231beb7c6a3f95d0ae4
saeed <saeed.da1994@gmail.com>
2022-10-02 16:24:39 +0330

    implement shell base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   cli:string = '"@angular/cli": "~13.2.5"';
-  core:string = '"@angular/cli": "~13.2.5"'
+  core:string = '"@angular/cli": "~13.2.5"';
 }
